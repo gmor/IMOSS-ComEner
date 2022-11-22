5d41e220b5e96f8e593706b72b37be467b33ad12
Gerard Mor Martinez <gerardmor@gmail.com>
2022-11-22 11:13:04 +0100

    Update executar_windows.cmd
    Normalment els nous usuaris d'R fan servir la versió més recent (4.2.2)

--- executar_windows.cmd
+++ executar_windows.cmd
@@ -1,2 +1,2 @@
 
-"C:\Program Files\R\R-4.0.3\bin\Rscript.exe" -e "rmarkdown::render('Codi\\NB.Rmd',output_format = rmarkdown::html_notebook(),output_file='..\\Veure_Resultats.html')"
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" -e "rmarkdown::render('Codi\\NB.Rmd',output_format = rmarkdown::html_notebook(),output_file='..\\Veure_Resultats.html')"
